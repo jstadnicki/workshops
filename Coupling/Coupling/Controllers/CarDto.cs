d425db200c4baf2ece0b8af9ddec6d57acb20d25
Jaroslaw Stadnicki <jaroslaw@revio.no>
2015-06-10 12:35:11 +0200

    002

---
+++ Coupling/Coupling/Controllers/CarDto.cs
@@ -0,0 +1,21 @@
+namespace Coupling.Controllers
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class CarDto
+    {
+        public CarDto()
+        {
+        }
+        public int SelectedCarType { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(typeof(decimal),"1","1000000000")]
+        public decimal Price { get; set; }
+    }
+}
